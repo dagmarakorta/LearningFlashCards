018259776359d04c43632372af9040f458073772
Dagmara Korta <dagmara.korta@gmail.com>
2026-01-04 03:50:05 +0100

    Fix packages issues after adding Swagger

--- src/LearningFlashCards.Api/Program.cs
+++ src/LearningFlashCards.Api/Program.cs
@@ -1,17 +1,29 @@
 using LearningFlashCards.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "LearningFlashCards API",
+        Version = "v1"
+    });
+});
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "LearningFlashCards API v1");
+    });
 }
 
 app.UseHttpsRedirection();
