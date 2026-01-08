8db53580919de35a78a7e505a6dd906ec3e0fd99
Dagmara Korta <dagmara.korta@gmail.com>
2026-01-08 19:14:41 +0100

    Generate first simple UI

--- src/LearningFlashCards.Maui/MainPage.xaml.cs
+++ src/LearningFlashCards.Maui/MainPage.xaml.cs
@@ -1,24 +1,21 @@
-﻿namespace LearningFlashCards.Maui
+using System.Collections.ObjectModel;
+
+namespace LearningFlashCards.Maui
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        public ObservableCollection<DeckListItem> Decks { get; } = new();
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+
+            Decks.Add(new DeckListItem("Spanish Basics", "32 cards - last studied 2 days ago"));
+            Decks.Add(new DeckListItem("History Dates", "18 cards - new"));
+            Decks.Add(new DeckListItem("Biology Terms", "25 cards - 60% mastered"));
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        public record DeckListItem(string Name, string Summary);
     }
 }
